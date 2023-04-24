ceb29a1dff34179125673dbc97f57432c13cfd41
Eliottln <eliott.le-normand@etu.univ-lyon1.fr>
2023-04-24 11:24:20 +0200

    new colors

--- app/src/main/java/fr/event/eventify/ui/MainActivity.kt
+++ app/src/main/java/fr/event/eventify/ui/MainActivity.kt
@@ -2,11 +2,10 @@ package fr.event.eventify.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import fr.event.eventify.R
 import fr.event.eventify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    `
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
