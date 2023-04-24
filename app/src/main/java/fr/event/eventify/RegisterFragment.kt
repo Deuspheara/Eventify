65e64159bc7886bf099bf9a25a45b37dd99716de
AntoineMorey <85560122+AntoineMorey@users.noreply.github.com>
2023-04-24 11:25:04 +0200

    new fragment register

---
+++ app/src/main/java/fr/event/eventify/RegisterFragment.kt
@@ -0,0 +1,35 @@
+package fr.event.eventify
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RegisterFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            RegisterFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
