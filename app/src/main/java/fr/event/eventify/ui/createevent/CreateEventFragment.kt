984c6a7cf02a45a9e1753fe80c540610620bb094
AntoineMorey <85560122+AntoineMorey@users.noreply.github.com>
2023-04-26 11:24:26 +0200

    layout createevent

---
+++ app/src/main/java/fr/event/eventify/ui/createevent/CreateEventFragment.kt
@@ -0,0 +1,30 @@
+package fr.event.eventify.ui.createevent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.event.eventify.R
+
+
+class CreateEventFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_event, container, false)
+    }
+
+
+}
