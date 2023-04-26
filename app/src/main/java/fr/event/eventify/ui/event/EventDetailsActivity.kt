ee565f44fd8416dae60eadb1313c4fb94741fa94
Eliottln <eliott.le-normand@etu.univ-lyon1.fr>
2023-04-26 17:35:52 +0200

    event page v1

---
+++ app/src/main/java/fr/event/eventify/ui/event/EventDetailsActivity.kt
@@ -0,0 +1,12 @@
+package fr.event.eventify.ui.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.event.eventify.R
+
+class EventDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+}
