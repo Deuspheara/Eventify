ab0dfcb95f2bab9ab116b4c6ffc839082aca04ad
AntoineMorey <85560122+AntoineMorey@users.noreply.github.com>
2023-04-27 11:54:30 +0200

    datepicker on createevent and change description to multiline text

--- app/src/main/java/fr/event/eventify/ui/createevent/CreateEventFragment.kt
+++ app/src/main/java/fr/event/eventify/ui/createevent/CreateEventFragment.kt
@@ -1,6 +1,7 @@
 package fr.event.eventify.ui.createevent
 
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.graphics.ImageDecoder
 import android.os.Bundle
@@ -8,16 +9,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import fr.event.eventify.R
 import fr.event.eventify.databinding.FragmentCreateEventBinding
 import fr.event.eventify.utils.ImageDialog
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 
-class CreateEventFragment : Fragment() {
+class CreateEventFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
     private lateinit var binding: FragmentCreateEventBinding
+    private var cal = Calendar.getInstance()
 
     private lateinit var startForEventImageResult: ActivityResultLauncher<Intent>
 
@@ -55,6 +62,22 @@ class CreateEventFragment : Fragment() {
             }
         }
 
+        binding.tfDateEvent.setOnClickListener{
+            DatePickerDialog(this.requireContext(), R.style.datepicker, this, cal.get(Calendar.YEAR), cal.get(
+                Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+    }
+
+    private fun updateDateInView() {
+        binding.tfDateEvent.setText(SimpleDateFormat("dd MMM yyyy", Locale.getDefault()).format(cal.time))
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        cal.set(Calendar.YEAR, year)
+        cal.set(Calendar.MONTH, month)
+        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+        updateDateInView()
     }
 
 
