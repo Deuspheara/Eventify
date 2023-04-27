bc0a82891fbaf7f7a3f4c5cf7a833614e1fb2e1e
AntoineMorey <85560122+AntoineMorey@users.noreply.github.com>
2023-04-27 09:53:20 +0200

    img picker create event

--- app/src/main/java/fr/event/eventify/ui/createevent/CreateEventFragment.kt
+++ app/src/main/java/fr/event/eventify/ui/createevent/CreateEventFragment.kt
@@ -1,15 +1,25 @@
 package fr.event.eventify.ui.createevent
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.ImageDecoder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import fr.event.eventify.R
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import fr.event.eventify.databinding.FragmentCreateEventBinding
+import fr.event.eventify.utils.ImageDialog
 
 
 class CreateEventFragment : Fragment() {
 
+    private lateinit var binding: FragmentCreateEventBinding
+
+    private lateinit var startForEventImageResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +32,29 @@ class CreateEventFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_event, container, false)
+
+        binding = FragmentCreateEventBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imgCreateEvent.setOnClickListener{
+            ImageDialog.takePicture(startForEventImageResult, this.requireActivity())
+        }
+
+        startForEventImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.data?.let { fileUri ->
+                    val source = ImageDecoder.createSource(requireContext().contentResolver, fileUri)
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    binding.imgCreateEvent.setImageBitmap(bitmap)
+                }
+            }
+        }
+
     }
 
 
