5def75c573f04f66920d63c3a3a7a0b3ddb5feac
Quentin Gaillardet <quentin.gaillardet01@gmail.com>
2023-05-02 23:18:21 +0200

    [WIP] test integration paypal - need login autorisation console android

--- app/src/main/java/fr/event/eventify/EventifyApp.kt
+++ app/src/main/java/fr/event/eventify/EventifyApp.kt
@@ -2,6 +2,12 @@ package fr.event.eventify
 
 import android.app.Application
 import android.content.Context
+import com.paypal.checkout.PayPalCheckout
+import com.paypal.checkout.config.CheckoutConfig
+import com.paypal.checkout.config.Environment
+import com.paypal.checkout.config.SettingsConfig
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.UserAction
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -9,5 +15,17 @@ import dagger.hilt.android.HiltAndroidApp
 class EventifyApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        val config = CheckoutConfig(
+            application = this,
+            clientId = BuildConfig.CLIENT_ID,
+            environment = Environment.SANDBOX,
+            returnUrl = "${BuildConfig.APPLICATION_ID}://paypalpay",
+            currencyCode = CurrencyCode.EUR,
+            userAction = UserAction.PAY_NOW,
+            settingsConfig = SettingsConfig(
+                loggingEnabled = true
+            )
+        )
+        PayPalCheckout.setConfig(config)
     }
 }
--- app/src/main/java/fr/event/eventify/ui/payment/PaymentSummaryFragment.kt
+++ app/src/main/java/fr/event/eventify/ui/payment/PaymentSummaryFragment.kt
@@ -1,11 +1,21 @@
 package fr.event.eventify.ui.payment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.paypal.checkout.approve.OnApprove
+import com.paypal.checkout.createorder.CreateOrder
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.OrderIntent
+import com.paypal.checkout.createorder.UserAction
+import com.paypal.checkout.order.Amount
+import com.paypal.checkout.order.AppContext
+import com.paypal.checkout.order.Order
+import com.paypal.checkout.order.PurchaseUnit
 import fr.event.eventify.databinding.FragmentPaymentSummaryBinding
 import fr.event.eventify.databinding.ItemSummaryBinding
 
@@ -22,6 +32,34 @@ class PaymentSummaryFragment : Fragment() {
         arguments?.let {
             numberOfParticipant = it.getInt("PARTICIPANT")
         }
+
+        binding.apply {
+            paymentButtonContainer.setup(
+                createOrder =
+                CreateOrder { createOrderActions ->
+                    val order =
+                        Order(
+                            intent = OrderIntent.CAPTURE,
+                            appContext = AppContext(userAction = UserAction.PAY_NOW),
+                            purchaseUnitList =
+                            listOf(
+                                PurchaseUnit(
+                                    amount =
+                                    Amount(currencyCode = CurrencyCode.EUR, value = "10.00")
+                                )
+                            )
+                        )
+                    createOrderActions.create(order)
+                },
+                onApprove =
+                OnApprove { approval ->
+                    approval.orderActions.capture { captureOrderResult ->
+                        Log.i("CaptureOrder", "CaptureOrderResult: $captureOrderResult")
+                    }
+                }
+            )
+        }
+
         return binding.root
     }
 
